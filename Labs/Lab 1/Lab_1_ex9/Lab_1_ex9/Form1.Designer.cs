5956297b68a24c7ba9c209a07b948cb48b950db5
GraemeDockrill <graeme.dockrill01@gmail.com>
2023-09-23 18:41:38 -0700

    Implemented scrolling background, player car control, collision with walls. Next to do: implement player collision with NPC cars

--- Labs/Lab 1/Lab_1_ex9/Lab_1_ex9/Form1.Designer.cs
+++ Labs/Lab 1/Lab_1_ex9/Lab_1_ex9/Form1.Designer.cs
@@ -32,18 +32,39 @@ namespace Lab_1_ex9
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.comboBoxCOMPorts = new System.Windows.Forms.ComboBox();
             this.buttonSerialConnection = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxAx = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBoxAy = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBoxAz = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBoxPlayerCar = new System.Windows.Forms.PictureBox();
+            this.pictureBoxBlueCar = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGreenCar = new System.Windows.Forms.PictureBox();
+            this.pictureBoxYellowCar = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRoad1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRoad2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGameBorder = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRoad3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxPurpleCar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayerCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlueCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreenCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYellowCar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGameBorder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPurpleCar)).BeginInit();
             this.SuspendLayout();
             // 
             // serialPort1
@@ -52,6 +73,7 @@ namespace Lab_1_ex9
             // 
             // timer1
             // 
+            this.timer1.Interval = 50;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // comboBoxCOMPorts
@@ -72,15 +94,6 @@ namespace Lab_1_ex9
             this.buttonSerialConnection.Text = "Connect Serial";
             this.buttonSerialConnection.UseVisualStyleBackColor = true;
             this.buttonSerialConnection.Click += new System.EventHandler(this.buttonSerialConnection_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 40);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(776, 784);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // label1
             // 
@@ -130,24 +143,176 @@ namespace Lab_1_ex9
             this.textBoxAz.Size = new System.Drawing.Size(100, 20);
             this.textBoxAz.TabIndex = 8;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Input X";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Input Y";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(69, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // pictureBoxPlayerCar
+            // 
+            this.pictureBoxPlayerCar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBoxPlayerCar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxPlayerCar.Image")));
+            this.pictureBoxPlayerCar.Location = new System.Drawing.Point(370, 128);
+            this.pictureBoxPlayerCar.MaximumSize = new System.Drawing.Size(100, 180);
+            this.pictureBoxPlayerCar.MinimumSize = new System.Drawing.Size(100, 180);
+            this.pictureBoxPlayerCar.Name = "pictureBoxPlayerCar";
+            this.pictureBoxPlayerCar.Size = new System.Drawing.Size(100, 180);
+            this.pictureBoxPlayerCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxPlayerCar.TabIndex = 13;
+            this.pictureBoxPlayerCar.TabStop = false;
+            // 
+            // pictureBoxBlueCar
+            // 
+            this.pictureBoxBlueCar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBoxBlueCar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxBlueCar.Image")));
+            this.pictureBoxBlueCar.Location = new System.Drawing.Point(179, 526);
+            this.pictureBoxBlueCar.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxBlueCar.Name = "pictureBoxBlueCar";
+            this.pictureBoxBlueCar.Size = new System.Drawing.Size(101, 182);
+            this.pictureBoxBlueCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxBlueCar.TabIndex = 14;
+            this.pictureBoxBlueCar.TabStop = false;
+            // 
+            // pictureBoxGreenCar
+            // 
+            this.pictureBoxGreenCar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBoxGreenCar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxGreenCar.Image")));
+            this.pictureBoxGreenCar.Location = new System.Drawing.Point(436, 526);
+            this.pictureBoxGreenCar.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxGreenCar.Name = "pictureBoxGreenCar";
+            this.pictureBoxGreenCar.Size = new System.Drawing.Size(100, 180);
+            this.pictureBoxGreenCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxGreenCar.TabIndex = 15;
+            this.pictureBoxGreenCar.TabStop = false;
+            // 
+            // pictureBoxYellowCar
+            // 
+            this.pictureBoxYellowCar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBoxYellowCar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxYellowCar.Image")));
+            this.pictureBoxYellowCar.Location = new System.Drawing.Point(301, 526);
+            this.pictureBoxYellowCar.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxYellowCar.Name = "pictureBoxYellowCar";
+            this.pictureBoxYellowCar.Size = new System.Drawing.Size(101, 182);
+            this.pictureBoxYellowCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxYellowCar.TabIndex = 16;
+            this.pictureBoxYellowCar.TabStop = false;
+            // 
+            // pictureBoxRoad1
+            // 
+            this.pictureBoxRoad1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRoad1.Image")));
+            this.pictureBoxRoad1.Location = new System.Drawing.Point(0, 630);
+            this.pictureBoxRoad1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBoxRoad1.Name = "pictureBoxRoad1";
+            this.pictureBoxRoad1.Size = new System.Drawing.Size(840, 650);
+            this.pictureBoxRoad1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxRoad1.TabIndex = 17;
+            this.pictureBoxRoad1.TabStop = false;
+            // 
+            // pictureBoxRoad2
+            // 
+            this.pictureBoxRoad2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRoad2.Image")));
+            this.pictureBoxRoad2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxRoad2.Name = "pictureBoxRoad2";
+            this.pictureBoxRoad2.Size = new System.Drawing.Size(840, 650);
+            this.pictureBoxRoad2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxRoad2.TabIndex = 19;
+            this.pictureBoxRoad2.TabStop = false;
+            // 
+            // pictureBoxGameBorder
+            // 
+            this.pictureBoxGameBorder.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxGameBorder.Location = new System.Drawing.Point(140, 0);
+            this.pictureBoxGameBorder.Name = "pictureBoxGameBorder";
+            this.pictureBoxGameBorder.Size = new System.Drawing.Size(700, 1200);
+            this.pictureBoxGameBorder.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxGameBorder.TabIndex = 20;
+            this.pictureBoxGameBorder.TabStop = false;
+            // 
+            // pictureBoxRoad3
+            // 
+            this.pictureBoxRoad3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRoad3.Image")));
+            this.pictureBoxRoad3.Location = new System.Drawing.Point(0, -630);
+            this.pictureBoxRoad3.Name = "pictureBoxRoad3";
+            this.pictureBoxRoad3.Size = new System.Drawing.Size(840, 650);
+            this.pictureBoxRoad3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxRoad3.TabIndex = 21;
+            this.pictureBoxRoad3.TabStop = false;
+            // 
+            // pictureBoxPurpleCar
+            // 
+            this.pictureBoxPurpleCar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBoxPurpleCar.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxPurpleCar.Image")));
+            this.pictureBoxPurpleCar.Location = new System.Drawing.Point(561, 526);
+            this.pictureBoxPurpleCar.Name = "pictureBoxPurpleCar";
+            this.pictureBoxPurpleCar.Size = new System.Drawing.Size(100, 180);
+            this.pictureBoxPurpleCar.TabIndex = 22;
+            this.pictureBoxPurpleCar.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 836);
+            this.ClientSize = new System.Drawing.Size(824, 1161);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBoxAz);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBoxAy);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBoxAx);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.buttonSerialConnection);
             this.Controls.Add(this.comboBoxCOMPorts);
+            this.Controls.Add(this.pictureBoxPlayerCar);
+            this.Controls.Add(this.pictureBoxPurpleCar);
+            this.Controls.Add(this.pictureBoxGreenCar);
+            this.Controls.Add(this.pictureBoxYellowCar);
+            this.Controls.Add(this.pictureBoxBlueCar);
+            this.Controls.Add(this.pictureBoxRoad1);
+            this.Controls.Add(this.pictureBoxRoad2);
+            this.Controls.Add(this.pictureBoxRoad3);
+            this.Controls.Add(this.pictureBoxGameBorder);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPlayerCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlueCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGreenCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxYellowCar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGameBorder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRoad3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPurpleCar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -159,15 +324,59 @@ namespace Lab_1_ex9
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ComboBox comboBoxCOMPorts;
         private System.Windows.Forms.Button buttonSerialConnection;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBoxAx;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBoxAy;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBoxAz;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBoxPlayerCar;
+        private System.Windows.Forms.PictureBox pictureBoxBlueCar;
+        private System.Windows.Forms.PictureBox pictureBoxGreenCar;
+        private System.Windows.Forms.PictureBox pictureBoxYellowCar;
+        private System.Windows.Forms.PictureBox pictureBoxRoad1;
+        private System.Windows.Forms.PictureBox pictureBoxRoad2;
+        private System.Windows.Forms.PictureBox pictureBoxGameBorder;
+        private System.Windows.Forms.PictureBox pictureBoxRoad3;
+        private System.Windows.Forms.PictureBox pictureBoxPurpleCar;
 
         ConcurrentQueue<Int32> dataQueue = new ConcurrentQueue<Int32>();
+
+        Random rnd = new Random();
+
+        double AxScaled;
+        double AyScaled;
+        int redCarX = 0;
+        int redCarY = 0;
+        int blueCarX = 0;
+        int blueCarY = 0;
+        int greenCarX = 0;
+        int greenCarY = 0;
+        int yellowCarX = 0;
+        int yellowCarY = 0;
+        int purpleCarX = 0;
+        int purpleCarY = 0;
+        int road1X = 0;
+        int road1Y = 0;
+        int road2X = 0;
+        int road2Y = 0;
+        int road3X = 0;
+        int road3Y = 0;
+        int roadSpeed = 5;
+        int carBaseSpeed = 1;
+        int blueCarSpeed;
+        int greenCarSpeed;
+        int yellowCarSpeed;
+        int purpleCarSpeed;
+
+
+        bool collision = false;
+
+        int step = 5;
 
         enum parsingByte
         {
@@ -178,6 +387,7 @@ namespace Lab_1_ex9
         }
 
         parsingByte parsingState = parsingByte.start;
+        
     }
 }
 
--- Labs/Lab 1/Lab_1_ex9/Lab_1_ex9/Form1.cs
+++ Labs/Lab 1/Lab_1_ex9/Lab_1_ex9/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Diagnostics;
 
 namespace Lab_1_ex9
 {
@@ -37,6 +38,12 @@ namespace Lab_1_ex9
         private void Form1_Load(object sender, EventArgs e)
         {
             timer1.Start();
+
+            // randomize car speed
+            blueCarSpeed = carBaseSpeed * rnd.Next(1, 3);
+            greenCarSpeed = carBaseSpeed * rnd.Next(1, 3);
+            yellowCarSpeed = carBaseSpeed * rnd.Next(1, 3);
+            purpleCarSpeed = carBaseSpeed * rnd.Next(1, 3);
 
             comboBoxCOMPorts.Items.Clear();
             comboBoxCOMPorts.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());
@@ -92,7 +99,7 @@ namespace Lab_1_ex9
                     break;
                 }
 
-                // parsing data stream = look for start byte of 255
+                // parsing data stream = look for start byte of 255---------------------------
                 if (item == 255)
                 {
                     parsingState = parsingByte.start;
@@ -107,17 +114,133 @@ namespace Lab_1_ex9
                 {
                     textBoxAx.Text = item.ToString();
                     parsingState = parsingByte.Ay;
+                    AxScaled = (-1.0 / 25) * item + 5.0; // scale orientation of X axis to -1 to +1
+                    textBox1.Text = AxScaled.ToString();
                 }
                 else if (parsingState == parsingByte.Ay) // parsing Ay byte
                 {
                     textBoxAy.Text = item.ToString();
                     parsingState = parsingByte.Az;
+                    AyScaled = (1.0 / 25) * item - 5.0; // scale orientation of Y axis to -1 to +1
+                    textBox2.Text = AyScaled.ToString();
                 }
                 else if (parsingState == parsingByte.Az) // parsing Az byte
                 {
                     textBoxAz.Text = item.ToString();
                     parsingState = parsingByte.start;
                 }
+
+                // player movement with the MSP430 board-----------------------------------
+                int x = pictureBoxPlayerCar.Location.X;
+                int y = pictureBoxPlayerCar.Location.Y;
+                
+                // player collision with right edge
+                if (x > pictureBoxRoad1.Location.X + pictureBoxGameBorder.Width - pictureBoxPlayerCar.Width)
+                {
+                    x = pictureBoxRoad1.Location.X + pictureBoxGameBorder.Width - pictureBoxPlayerCar.Width;
+
+                    pictureBoxPlayerCar.Location = new Point(x, y);
+                }
+                // player collision with bottom edge
+                if (y > pictureBoxGameBorder.Location.Y + pictureBoxGameBorder.Height - pictureBoxPlayerCar.Height)
+                {
+                    y = pictureBoxGameBorder.Location.Y + pictureBoxGameBorder.Height - pictureBoxPlayerCar.Height;
+
+                    pictureBoxPlayerCar.Location = new Point(x, y);
+                }
+                // player collision with left edge
+                if (x < pictureBoxGameBorder.Location.X)
+                {
+                    x = pictureBoxGameBorder.Location.X;
+
+                    pictureBoxPlayerCar.Location = new Point(x, y);
+                }
+                // player collision with top edge
+                if (y < pictureBoxGameBorder.Location.Y)
+                {
+                    y = pictureBoxGameBorder.Location.Y;
+
+                    pictureBoxPlayerCar.Location = new Point(x, y);
+                }
+                
+                // draw player location
+                pictureBoxPlayerCar.Location = new Point(x + Convert.ToInt32(step * AxScaled), y + Convert.ToInt32(step * AyScaled));
+
+                // scrolling background-----------------------------------------
+                road1X = pictureBoxRoad1.Location.X;
+                road1Y = pictureBoxRoad1.Location.Y;
+                road2X = pictureBoxRoad2.Location.X;
+                road2Y = pictureBoxRoad2.Location.Y;
+                road3X = pictureBoxRoad3.Location.X;
+                road3Y = pictureBoxRoad3.Location.Y;
+
+                road1Y += roadSpeed;
+                road2Y += roadSpeed;
+                road3Y += roadSpeed;
+
+                if (road1Y > pictureBoxGameBorder.Location.Y + pictureBoxGameBorder.Height)
+                {
+                    road1Y = road3Y - 630;
+                }
+                if (road2Y > pictureBoxGameBorder.Location.Y + pictureBoxGameBorder.Height)
+                {
+                    road2Y = road1Y - 630;
+                }
+                if (road3Y > pictureBoxGameBorder.Location.Y + pictureBoxGameBorder.Height)
+                {
+                    road3Y = road2Y - 630;
+                }
+
+                pictureBoxRoad1.Location = new Point(road1X, road1Y);
+                pictureBoxRoad2.Location = new Point(road2X, road2Y);
+                pictureBoxRoad3.Location = new Point(road3X, road3Y);
+
+                // scrolling car movement------------------------------------------
+                blueCarX = pictureBoxBlueCar.Location.X;
+                blueCarY = pictureBoxBlueCar.Location.Y;
+                greenCarX = pictureBoxGreenCar.Location.X;
+                greenCarY = pictureBoxGreenCar.Location.Y;
+                yellowCarX = pictureBoxYellowCar.Location.X;
+                yellowCarY = pictureBoxYellowCar.Location.Y;
+                purpleCarX = pictureBoxPurpleCar.Location.X;
+                purpleCarY = pictureBoxPurpleCar.Location.Y;
+
+                blueCarY += blueCarSpeed;
+                greenCarY += greenCarSpeed;
+                yellowCarY += yellowCarSpeed;
+                purpleCarY += purpleCarSpeed;
+
+                
+                // respawning cars when they go off the screen
+                if(blueCarY > pictureBoxGameBorder.Location.Y + pictureBoxGameBorder.Height)
+                {
+                    blueCarY = pictureBoxGameBorder.Location.Y - pictureBoxBlueCar.Height;
+                    blueCarSpeed = carBaseSpeed * rnd.Next(2, 4);
+                }
+                if (greenCarY > pictureBoxGameBorder.Location.Y + pictureBoxGameBorder.Height)
+                {
+                    greenCarY = pictureBoxGameBorder.Location.Y - pictureBoxGreenCar.Height;
+                    greenCarSpeed = carBaseSpeed * rnd.Next(2, 4);
+                }
+                if (yellowCarY > pictureBoxGameBorder.Location.Y + pictureBoxGameBorder.Height)
+                {
+                    yellowCarY = pictureBoxGameBorder.Location.Y - pictureBoxYellowCar.Height;
+                    yellowCarSpeed = carBaseSpeed * rnd.Next(2, 4);
+                }
+                if (purpleCarY > pictureBoxGameBorder.Location.Y + pictureBoxGameBorder.Height)
+                {
+                    purpleCarY = pictureBoxGameBorder.Location.Y - pictureBoxPurpleCar.Height;
+                    purpleCarSpeed = carBaseSpeed * rnd.Next(2, 4);
+                }
+
+                pictureBoxBlueCar.Location = new Point(blueCarX, blueCarY);
+                pictureBoxGreenCar.Location = new Point(greenCarX, greenCarY);
+                pictureBoxYellowCar.Location = new Point(yellowCarX, yellowCarY);
+                pictureBoxPurpleCar.Location = new Point(purpleCarX, purpleCarY);
+
+                // collision with cars----------------------------------------
+
+
             }
         }
     }
