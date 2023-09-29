fe30c85a0d1c2d58c541d5faf01ce1cb3c40d299
GraemeDockrill <graeme.dockrill01@gmail.com>
2023-09-29 00:40:03 -0700

    Completed Lab2 ex2

--- code_composer_workspace/Lab2_ex2/main.c
+++ code_composer_workspace/Lab2_ex2/main.c
@@ -11,31 +11,39 @@ int main(void)
     // initial setup
     CSCTL0 = CSKEY; // give clock password
 
-    // configuring DCO to run at 8 MHz
-    // CSCTL1 |= 0x0006;
-    CSCTL1 |= DCOFSEL_3;
+    // configure pins PJ.0, PJ.1, PJ.2, PJ.3, P3.4, P3.5, P3.6, P3.7 as digital outputs
 
-    // select SMCLK source
-    //CSCTL2 |= SELM__DCOCLK; // this was MCLK
-    CSCTL2 |= SELS__DCOCLK;
+    // set the P3.4 - P3.7 pins to outputs
+    P3DIR |= (BIT4 | BIT5 | BIT6 | BIT7); // or equals the BITs makes them outputs
 
-    // set up with a divider of 32
-    CSCTL3 |= DIVS__32;
+    // choose P3.4 - P3.7 pins to be general purpose I/O
+    P3SEL0 &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+    P3SEL1 &= ~(BIT4 | BIT5 | BIT6 | BIT7);
 
-    // configure Pin 3.4 as output - go to family guide pg 314 and look for PxDIR
-    //P3DIR |= BIT4 | BIT5; // the | BIT5 is for further exercises of blinking multiple LEDs
-    //P3SEL1 &= ~(BIT4 | BIT5);
-    //P3SEL0 &= ~(BIT4 | BIT5);
+    //set PJ.0-PJ.3 pins to be outputs
+    PJDIR |= (BIT0 | BIT1 | BIT2 | BIT3);
 
-    // configure Pin 3.4 as output
-    P3DIR |= BIT4; // setting pin 3.4 as an output
-    // choosing GPIO for pin 3.4
-    P3SEL1 |= BIT4; // setting only BIT4 of P3SEL1 register to 0
-    P3SEL0 |= BIT4; // setting only BIT4 of P3SEL0 register to 0
+    // choose PJ.0-PJ.3 pins to be general purpose I/O
+    PJSEL0 &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+    PJSEL1 &= ~(BIT0 | BIT1 | BIT2 | BIT3);
 
+    // setting output of LEDs to 10010011
+    PJOUT |= (BIT0 | BIT3);
+    PJOUT &= ~(BIT1 | BIT2);
+    P3OUT |= (BIT6 | BIT7);
+    P3OUT &= ~(BIT4 | BIT5);
 
     while(1){
-        _NOP();
+
+        int i;
+        // delay loop - do nothing
+        for(i = 0; i < 30000; i++){
+            _NOP();
+        }
+
+        // toggle LEDS 2,3,5,6
+        PJOUT ^= (BIT1 | BIT2);
+        P3OUT ^= (BIT4 | BIT5);
     }
 
     return 0;
