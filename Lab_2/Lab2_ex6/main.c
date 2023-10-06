60511accfd0f84f7c0d5df07e8be50d29c1e2466
GraemeDockrill <graeme.dockrill01@gmail.com>
2023-10-06 11:09:23 -0700

    Changing Lab2 ex6 to use P1.2 instead of P1.6 and switching edge trigger

--- Lab_2/Lab2_ex6/main.c
+++ Lab_2/Lab2_ex6/main.c
@@ -4,6 +4,12 @@
 
 
 unsigned int Timer = 0;
+volatile unsigned int rising_edge;
+volatile unsigned int falling_edge;
+volatile unsigned int edge_state = 0;
+volatile unsigned int difference = 0;
+
+
 
 /**
  * main.c
@@ -17,15 +23,15 @@ int main(void)
     // configuring clocks
     CSCTL0 = CSKEY;                     // unlocking clock
     CSCTL1 |= DCOFSEL1 + DCOFSEL0;      // set DCO to 8MHz
-    CSCTL2 = SELA_3 + SELS_3 + SELM_3;  // ACLK = DCO, SMCLK = DCO, MCLK = DCO
-    CSCTL3 = DIVA_4 + DIVS_4 + DIVM_4;  // ACLK/16, SMCLK/16, MCLK/16
+    CSCTL2 |= SELA_3 + SELS_3 + SELM_3;  // ACLK = DCO, SMCLK = DCO, MCLK = DCO
+    CSCTL3 |= DIVA_4 + DIVS_4 + DIVM_4;  // ACLK/16, SMCLK/16, MCLK/16
 
     // setting up Timer B
     TB1CTL |= TBSSEL1;                  // TB1 using SMCLK
     TB1CTL &= ~TBSSEL0;                 // TB1 using SMCLK
     TB1CTL &= ~(ID0 + ID1);             // TB1 with a CLK divider of 1
     TB1CTL |= MC0;                      // setting TB to up mode
-    TB1CTL |= TBIE;                     // enable TB1CTL interrupt
+    //TB1CTL |= TBIE;                     // enable TB1CTL interrupt
 
     // setting TB1.1 & TB1.2 out mode
     TB1CCTL1 = OUTMOD_7;                // set mode to Reset/Set
@@ -47,15 +53,26 @@ int main(void)
     // EXERCISE 6 CODE-------------------
 
     // setting up Timer A
-    TA0CTL |= TASSEL_2 + ID_0
-              + MC_1 + TAIE;          // TA1 using SMCLK, CLK divider of 1, set to up mode, enabing interrupt
-    TA0CCTL0 |= CCIS_0 + CM_3 + CAP
-              + CCIE + SCS + SCCI;      // capture input from CCI0A, capture on rising and falling edges, capture mode, interrupt enabled, synchronous capture,
+//    TA0CTL |= TASSEL_2 + ID_0 + TACLR
+//              + MC_2 + TAIE;          // TA1 using SMCLK, CLK divider of 1, set to up mode, enabing interrupt
+//    TA0CCTL0 |= CCIS_0 + CM_2 + CAP
+//              + CCIE + SCS;      // capture input from CCI0A, capture on rising and falling edges, capture mode, interrupt enabled, synchronous capture,
+
+    TA1CTL = TASSEL_2 + MC_2 + TACLR + TAIE;
+    TA1CCTL1 = CM_1 + CCIS_0 + CAP + SCS + CCIE;
+    TA1CCTL1 = CM_1 + CCIS_0 + CAP + SCS + CCIE;
+
+
 
     // setting up P1.6 for input Timer A
-    P1DIR &= ~BIT6;
-    P1SEL0 |= BIT6;
-    P1SEL1 |= BIT6;
+//    P1DIR &= ~BIT6;
+//    P1SEL0 |= BIT6;
+//    P1SEL1 |= BIT6;
+
+    //p1.2
+    P1DIR &= BIT2;
+    P1SEL0 |= BIT2;
+    P1SEL1 &= ~BIT2;
 
     // Global interrupt enable
     _EINT();
@@ -67,15 +84,60 @@ int main(void)
 }
 
 
-#pragma vector = TIMER0_A0_VECTOR       // interrupt vector for TA0 interrupt
-__interrupt void TA0_ISR(void)
+//#pragma vector = TIMER0_A0_VECTOR       // interrupt vector for TA0 interrupt
+//__interrupt void TA0_ISR(void)
+//{
+//    if(edge_state == 0){
+//        rising_edge = TA0CCR0;
+//        edge_state = 1;
+//        //TA0R = 0x0000;                  // reset counter to 0
+//
+//    }
+//    else if (edge_state == 1){
+//        //Timer = TA0CCR1;                   // store current time in Timer variable
+//        falling_edge = TA0CCR0;
+//        edge_state = 0;
+//
+//        if (falling_edge - rising_edge > 0)
+//            difference = falling_edge - rising_edge;
+//
+//    }
+//    //TA0CCTL0 &= ~CCIFG;                 // reset interrupt flag
+//    TA0CTL &= ~TAIFG;
+//}
+
+
+
+
+
+#pragma vector = TIMER1_A1_VECTOR       // interrupt vector for TA0 interrupt
+__interrupt void TA1_ISR(void)
 {
-    if(TA0CCTL0 & CCI){
-        TA0R = 0x0000;                  // reset counter to 0
+    switch (TA1IV)
+    {
+        case TA1IV_TACCR1:
+        {
+            if(edge_state == 0){
+                    rising_edge = TA1CCR1;
+                    edge_state = 1;
+                    TA1CCTL1 = CM_2 + CCIS_0 + CAP + SCS + CCIE;
+                    //TA0R = 0x0000;                  // reset counter to 0
+
+                }
+                else if (edge_state == 1){
+                    //Timer = TA0CCR1;                   // store current time in Timer variable
+                    falling_edge = TA1CCR1;
+                    edge_state = 0;
+                    TA1CCTL1 = CM_1 + CCIS_0 + CAP + SCS + CCIE;
+
+                    if (falling_edge - rising_edge > 0)
+                        difference = falling_edge - rising_edge;
+
+                }
+                //TA0CCTL0 &= ~CCIFG;                 // reset interrupt flag
+            TA0CTL &= ~TAIFG;
+            break;
+        }
     }
-    else{
-        Timer = TA0CCR1;                   // store current time in Timer variable
-    }
-    TA0CCTL0 &= ~CCIFG;                 // reset interrupt flag
-    TA0CTL &= ~TAIFG;
+
 }
