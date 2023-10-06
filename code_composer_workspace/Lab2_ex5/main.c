5e57e94fdfbeab02742172a92de4d69b3fb90f4e
GraemeDockrill <graeme.dockrill01@gmail.com>
2023-10-05 19:27:02 -0700

    Started lab2 exercise 5 (working with TimerB to set up a square wave)

---
+++ code_composer_workspace/Lab2_ex5/main.c
@@ -0,0 +1,64 @@
+#include <msp430.h> 
+
+// MECH 423 Lab 2 Exercise 5
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+	// configuring clocks
+	CSCTL0 = CSKEY;                     // unlocking clock
+	CSCTL1 |= DCOFSEL1 + DCOFSEL0;      // set DCO to 8MHz
+	CSCTL2 |= SELA0 + SELA1 + SELA2;    // ACLK = DCO
+	CSCTL2 |= SELS0 + SELS1 + SELS2;    // SMCLK = DCO
+	CSCTL2 |= SELM0 + SELM1 + SELM2;    // MCLK = DCO
+	CSCTL3 |= DIVA0 + DIVA1;            // ACLK/8
+	CSCTL3 |= DIVS0 + DIVS1;            // SMCLK/8
+	CSCTL3 |= DIVM0 + DIVM1;            // MCLK/8
+
+	// setting up Timer B
+	TB1CTL |= TBSSEL1;                  // TB1 using SMCLK
+	TB1CTL &= ~TBSSEL0;                 // TB1 using SMCLK
+	TB1CTL &= ~(ID0 + ID1);             // TB1 with a CLK divider of 1
+	TB1CTL |= MC0;                      // setting TB to up mode
+	TB1CTL |= TBIE;                     // enable TB1CTL interrupt
+
+	// setting TB1.1 cycle to 500Hz
+	TB1CCTL1 |= OUTMOD2;                // set mode to Toggle
+	TB1CCTL1 &= ~(OUTMOD0 + OUTMOD1);   // set mode to Toggle
+	TB1CCR0 = 499;                      // setting compare latch TB1CL0 - CAN'T WRITE DIRECTLY TO TB1CL0
+	TB1CCR1 = 249;                      // setting compare latch TB1CL1 - CAN'T WRITE DIRECTLY TO TB1CL0
+
+//	TB1CL0 = 0x01F3;                   // 499 in hex CAN'T WRITE DIRECTLY TO TBxCLn
+//	TB1CL1 = 0x00F9;                   // 249 in hex
+
+//	// setting TB1.1 cycle to 500Hz
+//    TB1CCTL2 |= OUTMOD2;                // set mode to Toggle
+//    TB1CCTL2 &= ~(OUTMOD0 + OUTMOD1);   // set mode to Toggle
+
+
+	//TB1CL0 = 499;
+
+	// setting up P3.4 as TB1.1 output
+	P3DIR |= BIT4;                      // setting P3.4 as output
+	P3SEL0 |= BIT4;                     // setting P3.4 as TB1.1
+	P3SEL1 &= ~BIT4;                    // setting P3.4 as TB1.1
+
+	// setting up P3.5 as TB1.2 output
+	P3DIR |= BIT5;                      // setting P3.5 as output
+	P3SEL0 |= BIT5;                     // setting P3.5 as TB1.2
+	P3SEL1 &= ~BIT5;                    // setting P3.4 as TB1.2
+
+
+
+
+	// setting up square wave
+
+	
+	while(1);
+
+	return 0;
+}
