fb2715343253597414448e9eba88e08001e3b964
graemedd@student.ubc.ca <graemedd@student.ubc.ca>
2023-10-12 16:21:22 -0700

    Added UART communication to Lab2 ex9

--- Lab_2/Lab2_ex9/main.c
+++ Lab_2/Lab2_ex9/main.c
@@ -1,6 +1,39 @@
 #include <msp430.h> 
 
+// defines and global variables
 #define BUFFER_SIZE 50
+
+// Setup Functions--------------------------------
+void clockSetup (void){
+    // configuring clocks
+    CSCTL0 = CSKEY;                             // unlocking clock
+    CSCTL1 |= DCOFSEL1 + DCOFSEL0;              // set DCO to 8MHz
+    CSCTL2 = SELA_3 + SELS_3 + SELM_3;          // ACLK = DCO, SMCLK = DCO, MCLK = DCO
+    CSCTL3 = DIVS__8 + DIVM__8;                // SMCLK/8, MCLK/8
+}
+
+void UART_Setup (void){
+    // Configure P2.0 and P2.1 ports for UART
+    P2SEL0 &= ~(BIT0 + BIT1);               // secondary module function UCA0RXD
+    P2SEL1 |= BIT0 + BIT1;                  // secondary module function UCA0TXD
+    UCA0CTLW0 |= UCSWRST;                   // Put the UART in software reset so can be modified
+    UCA0CTLW0 |= UCSSEL0;                   // Run the UART using ACLK
+    UCA0MCTLW = UCOS16 + UCBRF0 + 0x4900;   // Enable oversampling, Baud rate = 9600 from an 8 MHz clock (BRCLK) and from column UCBRSx
+    UCA0BRW = 52;                           // Clock prescaler from Table 18-5 column UCBRx
+    UCA0CTLW0 &= ~UCSWRST;                  // release UART for operation
+    UCA0IE |= UCRXIE;                       // Enable UART Rx interrupt
+}
+
+void UART_Tx (unsigned char TxByte){
+    while (!(UCA0IFG & UCTXIFG));       // wait until the previous Tx is finished
+    UCA0TXBUF = TxByte;                 // send TxByte
+}
+
+char UART_Rx (void){
+    unsigned char RxByte = 0;               // Create an unsigned character variable (8 bits)
+    RxByte = UCA0RXBUF;                     // Get the new byte from the Rx buffer
+    return RxByte;                          // return RxByte
+}
 
 typedef struct{
 
@@ -9,27 +42,50 @@ typedef struct{
     int tail;
     int full;
 
-} buffer;
+} CircularBuffer;
 
-void initializeBuffer(buffer* cb){
+// sets up buffer
+void initializeBuffer(CircularBuffer *cb){
     cb->head = 0;
     cb->tail = 0;
-    cb->full = 0;
+    cb->full = 0; // 1 = full
 }
 
-int isEmpty(const buffer *buffer{
+// checks if buffer is empty. returns cb->full
+int isEmpty(const CircularBuffer *cb){
+//    if(!cb->full && (cb->head == cb->tail))
+//        return 1;
+//    else
+//        return 0;
 
+    return cb->full;
 }
 
-void enqueue(buffer* cb, char data){
+// enqueues a char given and returns 1 if successful
+int enqueue(CircularBuffer *cb, char *data){
+    if(!cb->full){
+        cb->buffer[cb->head] = data;
+        cb->head = (cb->head + 1) % BUFFER_SIZE;    // warp around to beginning if overflows
+        if(cb->head == cb->tail)
+            cb->full = 1;                           // if buffer full, set full to 1
+        return 1;                                   // return 1 since successful
+    }
+    return 0;                                       // return 0 if unsuccessful
+}
 
-
+// dequeues a char from the buffer. returns 1 if works, 0 otherwise
+int dequeue(CircularBuffer *cb, char *data){
+    // if buffer is not empty
+    if(!isEmpty(cb)){
+        *data = cb->buffer[cb->tail];               // stores tail in data
+        cb->tail = (cb->tail + 1) % BUFFER_SIZE;     // advance tail and wrap around if overflow
+        cb->full = 0;                               // buffer no longer full
+        return 1;                                   // return 1 for success
+    }
+    return 0;                                       // return 0 for fail
 }
 
 
-char dequeue(buffer* cb){
-
-}
 
 /**
  * main.c
@@ -38,9 +94,38 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+	CircularBuffer cb;      // create buffer struct
+	initializeBuffer(&cb);  // initialize buffer at its memory location
 
+	// set up clock and UART
+	clockSetup();
+    UART_Setup();
 
+	// Global interrupt enable
+    _EINT();
 
+	while(1);
 
 	return 0;
+}
+
+
+#pragma vector = USCI_A0_VECTOR             // interrupt vector for Rx interrupt
+__interrupt void USCI_A0_ISR(void)
+{
+    // if carriage return received over UART, dequeue and send dequeued
+    if(UART_Rx() == 13){
+        dequeue
+    }
+
+    while (!(UCA0IFG & UCTXIFG));           // Wait until the previous Tx is finished
+    UCA0TXBUF = RxByte;                     // Echo back the received byte
+
+    while (!(UCA0IFG & UCTXIFG));           // Wait until the previous Tx is finished
+    UCA0TXBUF = RxByte + 1;                 // Echo back the received byte + 1
+
+    if(RxByte == 'j')                       // if received char is j, turn on LED
+        PJOUT |= BIT0;
+    if(RxByte == 'k')
+        PJOUT &= ~BIT0;                     // if received char is k, turn off LED
 }
