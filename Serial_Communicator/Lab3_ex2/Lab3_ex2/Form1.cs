3a4b610627c696e3a8ec90a8e3d5ffcf6a48e0e8
GraemeDockrill <graeme.dockrill01@gmail.com>
2023-10-31 13:46:34 -0700

    copying serial communicator before modifying it

---
+++ Serial_Communicator/Lab3_ex2/Lab3_ex2/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3_ex2
+{
+    public partial class Form1 : Form
+    {
+
+        public int numberOfDataPoints = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void ComPortUpdate()
+        {
+            cmbComPorts.Items.Clear();
+            string[] comPortArray = System.IO.Ports.SerialPort.GetPortNames();
+            Array.Reverse(comPortArray);
+            cmbComPorts.Items.AddRange(comPortArray);
+            if (cmbComPorts.Items.Count != 0)
+                cmbComPorts.SelectedIndex = 0;
+            else
+                cmbComPorts.Text = "No Ports Found!";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblDataRate.Visible = false;
+            cbComResponse.Checked = true;
+            cbByte1.Checked = false;
+            cbByte2.Checked = false;
+            cbByte3.Checked = false;
+            cbByte4.Checked = false;
+            cbByte5.Checked = false;
+            txtByte1.Enabled = false;
+            txtByte2.Enabled = false;
+            txtByte3.Enabled = false;
+            txtByte4.Enabled = false;
+            txtByte5.Enabled = false;
+            ComPortUpdate();
+        }
+
+        private void btnComConnect_Click(object sender, EventArgs e)
+        {
+            if (btnComConnect.Text == "Connect")
+            {
+                if(cmbComPorts.Items.Count > 0)
+                {
+                    try
+                    {
+                        serialPort1.BaudRate = Convert.ToInt16(txtBaudRate.Text);
+                        serialPort1.PortName = cmbComPorts.SelectedItem.ToString();
+                        serialPort1.Open();
+                        btnComConnect.Text = "Disconnect";
+                        timer1.Enabled = true;
+                        lblDataRate.Visible = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.Close();
+                    btnComConnect.Text = "Connect";
+                    timer1.Enabled = false;
+                    lblDataRate.Visible = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            while (serialPort1.IsOpen && serialPort1.BytesToRead != 0)
+            {
+                int currentByte = serialPort1.ReadByte();
+                numberOfDataPoints++;
+                if (cbComResponse.Checked)
+                {
+                    this.BeginInvoke(new EventHandler(delegate
+                    {
+                        txtComOutput.AppendText(currentByte.ToString() + ", ");
+                    }));
+                }
+            }
+        }
+
+        private void btnComTransmit_Click(object sender, EventArgs e)
+        {
+            byte[] TxBytes = new Byte[5];
+
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    if(cbByte1.Checked && (txtByte1.Text) != "")
+                    {
+                        TxBytes[0] = Convert.ToByte(txtByte1.Text);
+                        serialPort1.Write(TxBytes, 0, 1);
+                    }
+                    if (cbByte2.Checked && (txtByte2.Text) != "")
+                    {
+                        TxBytes[0] = Convert.ToByte(txtByte2.Text);
+                        serialPort1.Write(TxBytes, 0, 1);
+                    }
+                    if (cbByte3.Checked && (txtByte3.Text) != "")
+                    {
+                        TxBytes[0] = Convert.ToByte(txtByte3.Text);
+                        serialPort1.Write(TxBytes, 0, 1);
+                    }
+                    if (cbByte4.Checked && (txtByte4.Text) != "")
+                    {
+                        TxBytes[0] = Convert.ToByte(txtByte4.Text);
+                        serialPort1.Write(TxBytes, 0, 1);
+                    }
+                    if (cbByte5.Checked && (txtByte5.Text) != "")
+                    {
+                        TxBytes[0] = Convert.ToByte(txtByte5.Text);
+                        serialPort1.Write(TxBytes, 0, 1);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Byte_TextChanged(object sender, EventArgs e)
+        {
+            TextBox currentTextBox = sender as TextBox;
+            short parseResult;
+            if (Int16.TryParse((currentTextBox.Text), out parseResult))
+            {
+                if (parseResult > 255)
+                    parseResult = 255;
+                if (parseResult <= 0)
+                    parseResult = 0;
+                currentTextBox.Text = parseResult.ToString();
+            }
+            else
+                currentTextBox.Text = "";
+        }
+
+        private void cbByte1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbByte1.Checked == true)
+                txtByte1.Enabled = true;
+            else
+            {
+                txtByte1.Clear();
+                txtByte1.Enabled = false;
+            }
+        }
+
+        private void cbByte2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbByte2.Checked == true)
+                txtByte2.Enabled = true;
+            else
+            {
+                txtByte2.Clear();
+                txtByte2.Enabled = false;
+            }
+        }
+
+        private void cbByte3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbByte3.Checked == true)
+                txtByte3.Enabled = true;
+            else
+            {
+                txtByte3.Clear();
+                txtByte3.Enabled = false;
+            }
+        }
+
+        private void cbByte4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbByte4.Checked == true)
+                txtByte4.Enabled = true;
+            else
+            {
+                txtByte4.Clear();
+                txtByte4.Enabled = false;
+            }
+        }
+
+        private void cbByte5_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbByte5.Checked == true)
+                txtByte5.Enabled = true;
+            else
+            {
+                txtByte5.Clear();
+                txtByte5.Enabled = false;
+            }
+        }
+
+        private void cmbComPorts_DropDown(object sender, EventArgs e)
+        {
+            ComPortUpdate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDataRate.Text = "Incoming data rate = " + numberOfDataPoints.ToString() + " bytes per second";
+
+            if(cbAutoReconnect.Checked == true && numberOfDataPoints == 0 && serialPort1.IsOpen == true)
+            {
+                try
+                {
+                    serialPort1.Close();
+                    serialPort1.BaudRate = Convert.ToInt16(txtBaudRate.Text);
+                    serialPort1.PortName = cmbComPorts.SelectedItem.ToString();
+                    serialPort1.Open();
+                }
+                catch (Exception ex)
+                {
+                    //MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
