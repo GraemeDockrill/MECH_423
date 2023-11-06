380bfdaef19f27eb916874972a9f23410763428c
GraemeDockrill <graeme.dockrill01@gmail.com>
2023-11-06 14:00:15 -0800

    Fixed stepper speed to run slower at lower velocity commands

--- Lab_3/Lab3_ex3_graeme/main.c
+++ Lab_3/Lab3_ex3_graeme/main.c
@@ -9,6 +9,7 @@ unsigned volatile char cmdByte1;
 unsigned volatile char stepSpeedByte;
 unsigned volatile char dutyByte;
 unsigned volatile int parsingMessage = 0;
+unsigned volatile int newCommand = 0;      // if a message received on UART, set = 1
 unsigned volatile int halfStepState = 0;
 unsigned volatile int stepMode = 1;         // 1 = whole stepping, 0 = half stepping
 unsigned volatile int stepDir = 1;          // 1 = CW, 0 = CCW
@@ -49,7 +50,7 @@ int main(void)
 
     // setting up TB0 for stepper stepping
     TB0CTL |= TBSSEL__ACLK;                     // TB0 using ACLK
-    TB0CTL |= ID__2;                            // TB0 with a divider of 1
+    TB0CTL |= ID__4;                            // TB0 with a divider of 1
     TB0CTL |= MC__UP;                            // TB0 in UP mode
     TB0CCR0 = stepSpeed;                        // set UP counter to 40000
     TB0CCTL0 |= CCIE;                           // enable interrupt for TB0
@@ -113,44 +114,10 @@ int main(void)
     // Global interrupt enable
     _EINT();
 
-    while(1);
+    // control loop for changing states
+    while(1){
 
-	return 0;
-}
-
-
-// -------- INTERRUPT SERVICE ROUTINES --------
-
-
-#pragma vector = USCI_A1_VECTOR             // interrupt vector for Rx interrupt
-__interrupt void USCI_A1_ISR(void)
-{
-    Rx = UART1_Rx();                         // get char from UART
-
-    if(parsingMessage == 1)
-        UART1_string("Currently parsing a byte!");
-    // ensure you get the start byte first
-    else if(cb->count == 0 && Rx == 255){
-        enqueue(cb, Rx);                        // enqueue the start byte
-    }
-    else if(cb->count > 0){
-        enqueue(cb, Rx);                        // enqueue the received byte
-    }
-
-    if(cb->count >= 5){
-        parsingMessage = 1;                     // signal we're parsing a message
-        startByte = dequeue(cb);
-        cmdByte0 = dequeue(cb);
-        cmdByte1 = dequeue(cb);
-        stepSpeedByte = dequeue(cb);
-        dutyByte = dequeue(cb);
-        UART1_Tx(startByte);
-        UART1_Tx(cmdByte0);
-        UART1_Tx(cmdByte1);
-        UART1_Tx(stepSpeedByte);
-        UART1_Tx(dutyByte);
-
-        if(startByte == 255){
+        if(newCommand){
             // turning duty cycle byte into motor speed
             motorSpeed = (65534.0 * dutyByte) / 100;
             TB2CCR2 = motorSpeed;               // changing motor speed
@@ -160,7 +127,7 @@ __interrupt void USCI_A1_ISR(void)
             else
                 stopStep = 0;
 
-            stepSpeed = -(50535 * stepSpeedByte) / 100 + 65535;
+            stepSpeed = -(58034 * stepSpeedByte) / 100 + 65535;
             TB0CCR0 = stepSpeed;                // changing stepper motor speed
 
             // stepper motor control based on cmdByte0
@@ -224,6 +191,124 @@ __interrupt void USCI_A1_ISR(void)
                 default:
                     break;
             }
+
+            newCommand = 0;                         // we've read the command
+        }
+
+    }
+
+	return 0;
+}
+
+
+// -------- INTERRUPT SERVICE ROUTINES --------
+
+
+#pragma vector = USCI_A1_VECTOR             // interrupt vector for Rx interrupt
+__interrupt void USCI_A1_ISR(void)
+{
+    Rx = UART1_Rx();                         // get char from UART
+
+    if(parsingMessage == 1)
+        UART1_string("Currently parsing a byte!");
+    // ensure you get the start byte first
+    else if(cb->count == 0 && Rx == 255){
+        enqueue(cb, Rx);                        // enqueue the start byte
+    }
+    else if(cb->count > 0){
+        enqueue(cb, Rx);                        // enqueue the received byte
+    }
+
+    if(cb->count >= 5){
+        parsingMessage = 1;                     // signal we're parsing a message
+        startByte = dequeue(cb);
+        cmdByte0 = dequeue(cb);
+        cmdByte1 = dequeue(cb);
+        stepSpeedByte = dequeue(cb);
+        dutyByte = dequeue(cb);
+        UART1_Tx(startByte);
+        UART1_Tx(cmdByte0);
+        UART1_Tx(cmdByte1);
+        UART1_Tx(stepSpeedByte);
+        UART1_Tx(dutyByte);
+
+        if(startByte == 255){
+
+            newCommand = 1;
+
+//            // turning duty cycle byte into motor speed
+//            motorSpeed = (65534.0 * dutyByte) / 100;
+//            TB2CCR2 = motorSpeed;               // changing motor speed
+//
+//            if(stepSpeedByte == 0)
+//                stopStep = 1;
+//            else
+//                stopStep = 0;
+//
+//            stepSpeed = -(50535 * stepSpeedByte) / 100 + 65535;
+//            TB0CCR0 = stepSpeed;                // changing stepper motor speed
+//
+//            // stepper motor control based on cmdByte0
+//            switch(cmdByte0){
+//                case 0:                             // whole step CW
+//                    stepMode = 1;
+//                    incrementStep();
+//                    TB0CCTL0 &= ~CCIE;              // disable interrupt for TB0
+//                    break;
+//                case 1:                             // whole step CCW
+//                    stepMode = 1;
+//                    decrementStep();
+//                    TB0CCTL0 &= ~CCIE;              // disable interrupt for TB0
+//                    break;
+//                case 2:                             // half step CW
+//                    stepMode = 0;
+//                    incrementStep();
+//                    TB0CCTL0 &= ~CCIE;              // disable interrupt for TB0
+//                    break;
+//                case 3:                             // half step CCW
+//                    stepMode = 0;
+//                    decrementStep();
+//                    TB0CCTL0 &= ~CCIE;              // disable interrupt for TB0
+//                    break;
+//                case 4:                             // continuous whole step CW
+//                    stepMode = 1;
+//                    stepDir = 1;
+//                    TB0CCTL0 |= CCIE;               // enable interrupt for TB0
+//                    break;
+//                case 5:                             // continuous whole step CCW
+//                    stepMode = 1;
+//                    stepDir = 0;
+//                    TB0CCTL0 |= CCIE;               // enable interrupt for TB0
+//                    break;
+//                case 6:                             // continuous half step CW
+//                    stepMode = 0;
+//                    stepDir = 1;
+//                    TB0CCTL0 |= CCIE;               // enable interrupt for TB0
+//                    break;
+//                case 7:                             // continuous half step CCW
+//                    stepMode = 0;
+//                    stepDir = 0;
+//                    TB0CCTL0 |= CCIE;               // enable interrupt for TB0
+//                    break;
+//                default:
+//                    break;
+//            }
+//
+//            updateCoils();                      // update stepper coils based on new command
+//
+//            // DC motor control based on cmdByte1
+//            switch(cmdByte1){
+//                case 0:                             // CW DC motor
+//                    P3OUT &= ~BIT7;
+//                    P3OUT |= BIT6;
+//                    break;
+//                case 1:                             // CCW DC motor
+//                    P3OUT &= ~BIT6;
+//                    P3OUT |= BIT7;
+//                    break;
+//                default:
+//                    break;
+//            }
 
 
         }
